33d59c5ab04ab42a4c0be0ebe322f77db3a801c7
gdtknight <gdtknight@naver.com>
2022-10-31 19:22:50 +0900

    Setting up firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDTUFkIYnC7i7JSKwuUiUdCgrQZaKruHXU",
+  authDomain: "netflix-clone-a43d4.firebaseapp.com",
+  projectId: "netflix-clone-a43d4",
+  storageBucket: "netflix-clone-a43d4.appspot.com",
+  messagingSenderId: "665255362534",
+  appId: "1:665255362534:web:c06569d82e52be17562027"
+};
