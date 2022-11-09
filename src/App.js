2d882a2e3b94188dafd140970a3a005ef1756b22
gdtknight <gdtknight@naver.com>
2022-11-09 18:29:33 +0900

    Modify App.js to recognize user login

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const user = {
+    name: "sonny",
+  };
   return (
     <div className="app">
       <Router>
         <Routes>
-          <Route
-            exact path="/"
-            element={
-              <HomeScreen />
-            }
-          />
+          <Route exact path="/" element={(!user ? (<LoginScreen />) : (<HomeScreen />))} />
         </Routes>
       </Router>
     </div >
