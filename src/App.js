a219be8ce1a3347027f658ec07d0eac0bdaf2a3b
gdtknight <gdtknight@naver.com>
2022-11-12 22:55:12 +0900

    Add useEffect to consistence auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { auth } from "./firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log(user);
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        // const uid = user.uid;
+        // ...
+      } else {
+        console.log("something wrong");
+        // User is signed out
+        // ...
+      }
+    });
+
+    return unsubscribe;
+  });
   return (
     <div className="app">
       <Router>
