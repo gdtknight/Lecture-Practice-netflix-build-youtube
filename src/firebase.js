52aece5305166a25ace614c8c7a8aee19f60c9f5
gdtknight <gdtknight@naver.com>
2022-11-12 21:17:24 +0900

    Modify firebase.js
    const app, const db, const auth implemented by firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from 'firebase/auth';
+
 const firebaseConfig = {
   apiKey: "AIzaSyDTUFkIYnC7i7JSKwuUiUdCgrQZaKruHXU",
   authDomain: "netflix-clone-a43d4.firebaseapp.com",
@@ -6,3 +10,10 @@ const firebaseConfig = {
   messagingSenderId: "665255362534",
   appId: "1:665255362534:web:c06569d82e52be17562027"
 };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { auth };
+export default db;
