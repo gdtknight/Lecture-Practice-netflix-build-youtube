b19096bb6aa2b18633c6d818fec002eb11590abb
gdtknight <gdtknight@naver.com>
2022-11-12 22:09:24 +0900

    Add sign-up sign-in functionality

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,22 +1,55 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './SignupScreen.css';
 
+import { auth } from '../firebase';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+
 function SignupScreen() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
   const register = (e) => {
     e.preventDefault();
+
+    createUserWithEmailAndPassword(auth, emailRef.current.value, passwordRef.current.value)
+      .then((userCredential) => {
+        console.log(userCredential.user);
+      })
+      .catch((error) => {
+        console.log(error.message);
+        // const errorCode = error.code;
+        // const errorMessage = error.message;
+      });
   }
 
   const signIn = (e) => {
     e.preventDefault()
+
+    signInWithEmailAndPassword(auth, emailRef.current.value, passwordRef.current.value)
+      .then((userCredential) => {
+        console.log(userCredential.user);
+      })
+      .catch((error) => {
+        console.log(error.message);
+        // const errorCode = error.code;
+        // const errorMessage = error.message;
+      });
   }
+
+  signOut(auth).then(() => {
+    // Sign-out successful.
+  }).catch((error) => {
+    // An error happened.
+  });
+
 
   return (
     <div className="signupScreen">
       <form>
         <h1>Sign In</h1>
-        <input placeholder="Email" type="email" />
-        <input placeholder="Password" type="password" />
-        <button type="submit">Sign In</button>
+        <input ref={emailRef} placeholder="Email" type="email" />
+        <input ref={passwordRef} placeholder="Password" type="password" />
+        <button type="submit" onClick={signIn}>Sign In</button>
 
         <h4>
           <span className="signupScreen__gray">New to Netflix? </span>
